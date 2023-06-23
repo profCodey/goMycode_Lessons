6eaca6758303fe9a981e434c23a003fcde069811
profCodey <otunaiyademilade@gmail.com>
2023-06-23 01:39:20 +0100

    recurssion

---
+++ recurssion.js
@@ -0,0 +1,64 @@
+let n = "20"
+//give a student 20 dollars
+//keep one dollars for himself and passes the rest to another student
+//give the next student 19 dollars
+//let him keep one dollar for himself
+//give the next person 18 dollars, let him keep one dollar for himself
+
+
+n =19
+//give 1st student 20
+
+// function chopIChop(n=20) {
+//   keep one dollar
+
+// if (n is not equals or less 1) {
+    //chopIChop(n = n-1)
+// }
+
+//keep the last one naira
+//return
+
+// }
+
+
+
+
+// A date is given as an integer of 6 digits. For example, the number 181019 represents October 18, 2019.
+// Develop an algorithm that has such a number as input, checks that this number corresponds to a valid date and displays the following result:
+// Day: 18
+// Month: October
+// Year: 2019
+
+//take in the 6 digit integer date as an  input
+//check if the date comprises of only integers
+//if not, throw an error - Date is not valid
+//check if the date is 6 digits
+//if not, throw an error - Date is not valid
+//if the first 2 digits is greater than 31 or if the first two is less than 01, throw an error - Date is not valid
+//if the 3rd and 4th digit put together is less that 01 or greater tahn 12, throw an error -date is invalid
+//if th 5th and 6th digit put together is less than 00 or greater than 99, throw an error - date is invalid
+//if the 3rd and 4th number is 04, 10, 06, 11 and the first two is greater 30, throw an error -date is invalid
+//if the 3rd and 4th number is 02, and the first two number is greater 28days, throw an error
+//if the last two is divisible by 4, and the 3rd and 4th is 02, and the first two is greater 29days, throw an error - invalid year
+
+
+
+// 220224
+
+
+//take in the 6 digit integer date as an  input
+//check that the date is valid
+//check that it displays the following
+// Day: 18
+// Month: October
+// Year: 2019
+
+
+
+
+let result = 7 + 5;
+console.log(result);
+
+let result2 = 5 + "5";
+console.log(typeof result2)
