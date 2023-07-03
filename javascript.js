c75a00b71f2f09262f3d0c7e8ecb84fb13304681
profCodey <otunaiyademilade@gmail.com>
2023-07-03 21:10:37 +0100

    algorithm solution

--- javascript.js
+++ javascript.js
@@ -178,13 +178,204 @@ console.log(drawer[0])
 //     i +=2
 // }
 
-let count = 1;
-while (count < 11) {
-    console.log("Halelluyah")
-    count += 1;
-}
+// let count = 1;
+// while (count < 11) {
+//     console.log("Halelluyah")
+//     count += 1;
+// }
 
 // for (let i = 0; i < 11; i++) {
 //     console.log("halleluyah")
 // }
 
+
+let count = 1; 
+while (count < 8) {
+    console.log("nifemi");
+    count++;
+}
+
+for (let count = 1; count < 8; count++){
+    console.log("nifemi");
+}
+
+for (let count = 0; count < 10; count++){
+    console.log("sola")
+}
+
+let parentAccounts = 1000000;
+let childAccounts = 0;
+let childAges = 5;
+for (let i = childAges; i < 18; i += 2){
+    parentAccounts = parentAccounts - 75000;
+    childAccounts = childAccounts + 75000;
+}
+
+console.log(parentAccounts)
+console.log(childAccounts)
+let sentence = "Tunde is rich";
+let names = "sola"
+names[1 + 1]
+
+let counts = 0;
+for (let i = 0; i <= sentence.length - 1; i += 1){
+    if (sentence[i] === " " && sentence[i + 1] !== " ") {
+            counts++;
+    }
+}
+
+console.log(counts + 1)
+
+//create a "count" varianble that will keep track of the number of space
+//create a for loop
+//if the current element is a space
+//if its a space and the next element is not a space
+//add the to count
+//else continue
+
+let fruits = ["mango", "orange", "banana", "apple"];
+let name = "sola   is a girl"
+
+
+let age = 10;
+let newAge = age.toString()
+newAge
+ 
+let receptAmt = (100 / 3)
+console.log(typeof receptAmt);
+
+console.log(Number(receptAmt) + 50 + 100);
+
+let result = receptAmt.toPrecision(4)
+console.log(receptAmt);
+console.log(result);
+
+console.log(typeof age);
+console.log(typeof newAge);
+let myName = "sola"
+let x = 10;
+let square = x.toExponential(2)
+console.log(x);
+console.log(square);
+
+
+
+
+let ten = "7.5" 
+
+console.log(Number(ten));
+console.log(parseInt(ten));
+console.log(parseFloat(ten));
+
+
+function getDayOfTheWeek(input) {
+    let day;
+    if (typeof input === String) {
+        throw new Error("Input is not a number")
+    }
+
+    switch (input) {
+      case 1:
+        day = "Monday";
+        break;
+
+      case 2:
+        day = "Tuesday";
+        break;
+
+      case 3:
+        day = "Wedensday";
+        break;
+
+      case 4:
+        day = "Thursday";
+        break;
+
+      case 5:
+        day = "Friday";
+            break;
+        
+      default:
+        day = "Not a work day";
+    }
+
+    return day
+}
+console.log(getDayOfTheWeek("sola"));
+
+
+function numberToString(num) {
+  // Return a string of the number here!
+
+    let result = num.toFixed(3);
+    return result
+}
+
+let answer = numberToString(23.555);
+console.log(answer);
+
+
+let lanreFirstName = "Lanre"
+let lanreLastName = "Olawunmi"
+let lanreFavColor = "Black"
+let lanreFavFood = "Beans"
+
+let lanre = {
+    firstName: "lanre",
+    LastName: "olawunmi",
+    favColor: "Black",
+    favFood: "beans"
+}
+//TO ACCESS THE PROPERTIES OF AN OBJECT IN JAVASCRIPT
+//dot notation - objName.key
+//bracket notation objName['key']
+
+let lname = lanre.LastName
+let lcolor = lanre.favColor
+
+console.log(lanre.LastName);
+  console.log(lcolor);
+
+let lanre2 = ["lanre", "olawunmi", "black", "beans"]
+lanre[3]
+
+console.log(lanre);
+
+          
+
+console.log(log());
+
+function log() {
+  return "I am happy";
+} 
+let arr = ["Keep","remove", "Keep", "Remove", "Keep"];
+let newArr = ["Keep", "Keep"];
+function removeEveryOther(arr) {
+  let newArr = []
+  //your code here
+  for (let i = 0; i < arr.length; i+=2) {
+    //If the current is in an even index
+   newArr.push(arr[i])
+  }
+  return newArr;
+}
+
+
+
+
+
+
+
+let ans1 = removeEveryOther(["Keep", "Remove", "Keep", "Remove", "Keep"])
+console.log(ans1);
+
+
+let classArr= [];
+classArr.push("Hakeeb")
+console.log(classArr);
+classArr.push("Dami")
+console.log(classArr);
+
+let myDreamCar = {
+  
+}
